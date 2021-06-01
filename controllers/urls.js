ef3a6eddf6c9a502221c8fcf696cd000209281f0
TarekGuesri <tguesri@gmail.com>
2021-06-01 19:57:18 +0100

    Completed encode controller

--- controllers/urls.js
+++ controllers/urls.js
@@ -1,14 +1,41 @@
-const { check, validationResult } = require('express-validator');
+const { validationResult } = require('express-validator');
+const shortid = require('shortid');
 
-exports.encode = (req, res, next) => {
-  console.log(req.body);
+const Url = require('../models/Url');
+
+exports.encode = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  res.json('Encode');
+
+  const { longUrl } = req.body;
+
+  let url = await Url.findOne({ longUrl });
+
+  // If there is a short link with same longUrl, we will just send it instead of ceating another one
+  if (url) {
+    return res.json(url.shortUrl);
+  } else {
+    const urlCode = shortid.generate();
+
+    const baseUrl = process.env.BASE_URL;
+
+    const shortUrl = `${baseUrl}/${urlCode}`;
+
+    // Creating model
+    url = new Url({
+      longUrl,
+      shortUrl,
+      urlCode,
+    });
+
+    await url.save();
+
+    return res.json(url.shortUrl);
+  }
 };
 
-exports.decode = (req, res, next) => {
+exports.decode = (req, res) => {
   res.json('Decode');
 };
--- routes/rest/urls.js
+++ routes/rest/urls.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { check, validationResult } = require('express-validator');
+const { check } = require('express-validator');
 
 const { encode, decode } = require('../../controllers/urls');
 
